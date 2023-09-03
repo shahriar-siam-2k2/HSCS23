8f4bc9d2609503cda4bd36885f865733ae4de726
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-09-03 21:04:19 +0600

    17-up

---
+++ 17-Marks Array/17-marksArr.cpp
@@ -0,0 +1,12 @@
+//17-marksArr.cpp
+//Question at 17-marksArr-Ques.txt
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[3] = {90, 88, 92};
+    cout << endl << "My English marks were: " << marks[0] << endl;
+    cout << "My Bangla marks were: " << marks[1] << endl;
+    cout << "My ICT marks were: " << marks[2] << endl;
+    return 0;
+}
