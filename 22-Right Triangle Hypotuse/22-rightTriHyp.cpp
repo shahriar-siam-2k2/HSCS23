6159cccaf80ad2b9606e90e52821dfa204bcf337
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-09-06 22:05:28 +0600

    Initial

---
+++ 22-Right Triangle Hypotuse/22-rightTriHyp.cpp
@@ -0,0 +1,12 @@
+//22-rightTriHyp.cpp
+//Question at 22-rightTriHyp-Ques.txt
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int a=5, b=10;
+    double hyp = sqrt((a*a)+(b*b));
+    cout << endl << "The arm lengt of the hypotenuse is " << hyp << endl;
+    return 0;
+}
