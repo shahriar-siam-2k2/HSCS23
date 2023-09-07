ebbd2503dc49019e386c2fd23b68ac858c2f0a63
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-09-07 20:49:58 +0600

    Initial

---
+++ 23-Power/23-power.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    double num1, a, b;
+    cout << endl;
+    cin >> a >> b;
+    num1 = pow(a, b);
+    cout << endl << "a to the power b is " << num1 << endl;
+    return 0;
+}
